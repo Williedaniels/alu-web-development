ae778f5c8b237c162f1b3bbcb26a9d645b1cd5ce
Williedaniels <w.daniels@alustudent.com>
2025-01-23 16:47:34 +0200

    updated index file

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -1,44 +1,22 @@
 #!/usr/bin/env python3
-""" Module of Index views
-"""
+""" Index view module """
 from flask import jsonify, abort
 from api.v1.views import app_views
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
-def status() -> str:
-    """ GET /api/v1/status
-    Return:
-      - the status of the API
-    """
-    return jsonify({"status": "OK"})
-
-
-@app_views.route('/stats/', strict_slashes=False)
-def stats() -> str:
-    """ GET /api/v1/stats
-    Return:
-      - the number of each objects
-    """
-    from models.user import User
-    stats = {}
-    stats['users'] = User.count()
-    return jsonify(stats)
-
-
 @app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
-    """
-    GET /api/v1/unauthorized
-    return unauthorized message
-    """
-    abort(401, description="UNAUTHORIZED")
+    """ Unauthorized endpoint """
+    abort(401)
 
 
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden() -> str:
-    """
-    GET /api/v1/forbidden
-    return 403 page
-    """
-    abort(403, description="FORBIDDEN")
+    """ Forbidden endpoint """
+    abort(403)
+
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """ Status of the API """
+    return jsonify({"status": "OK"})
